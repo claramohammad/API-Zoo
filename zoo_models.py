46d804261d1e166e7edcac11c7165e281265e51f
claramohammad <clara.mohammad@sou.inteli.edu.br>
2024-05-04 18:14:59 -0300

    Add: zoo models

---
+++ zoo_models.py
@@ -0,0 +1,36 @@
+class animal:
+    def __innit__(self, name, species):
+        self.name = name
+        self.species = species
+        self.hapiness = 0 #neutral
+    
+    def feed(self, value):
+        self.hapiness += value
+
+class enclosure:
+    def __innit__(self, species):
+        self.species = species
+        self.animals = []
+        self.conservation_status = "Bem cuidado"
+    
+    def change_status(self, new_status):
+        self.conservation_status = new_status
+
+    def add_animal(self, animal):
+        if animal.species == self.species:
+            self.animals.append(animal)
+        else:
+            raise ValueError("Especie do animal não compativel com o recinto")
+        
+class zoo:
+    def __innit__(self):
+        self.enclosures = []
+        self.money = 0
+
+    def add_enclosure(self, enclosure):
+        self.enclosures.append(enclosure)
+
+    def visitors(self, visitors_number):
+        hapiness.rate = sum(animal.hapiness for enclosure in self.enclosures for animal in enclosure.animals)
+        conservation_rate = sum(1 for enclosure in self.enclosures if enclosure.conservation_status == "Bem cuidado")
+        self.money += visitors_number * (hapiness_rate + conservation_rate)
